c1fe8e892375a7f2028c358403bbfa024153648f
Mary <littlemary1379@gmail.com>
2021-04-08 16:55:59 +0900

    set vector initlize

--- canvas.js
+++ canvas.js
@@ -1,7 +1,14 @@
 var mCanvas;
-var line;
+
+var line = line;
+
 var isDown;
+var renderingObject = false;
+var newLine;
+
+var lastPoint;
 var objectId = 0;
+
 
 window.onload = function drawOneLine(){
     
@@ -16,18 +23,28 @@ function initHTML(){
 function editMode(){
     console.log("편집모드 진입");
     //mCanvas.setActiveObject(mCanvas.item(objectId-1));
+
+    if(objectId == 0) {
+        console.log("랜더링 없음");
+        return;
+    } 
+
     removeSpot(mCanvas, objectId-1)
     objectId = objectId -1;
     mCanvas.renderAll();
+    console.log("편집모드 종료");
     
 }
 
 function removeSpot(canvas, id) {
+    
     canvas.forEachObject(function(obj) {
-        console.log("???????" + obj.id);
-        console.log("?????" + id);
+
+        console.log("받은 아이디"+id);
+        console.log("만들어진 오브젝트 아이디"+obj.id);
+
+        console.log(obj.id);
         if (obj.id == id) {
-            console.log("?????????????");
             canvas.remove(obj);
         };
     });
@@ -37,22 +54,56 @@ function setListener(){
     mCanvas.on('mouse:down', function(o){
 
         isDown = true
-
-        var pointer = mCanvas.getPointer(o.e)
-        console.log("마우스 다운" + pointer.x + " , " + pointer.y);
         
-        var point = [pointer.x, pointer.y, pointer.x, pointer.y]
-        line = new fabric.Line(point, {
-            id : objectId,
-            strokeWidth : 2,
-            fill : 'black',
-            stroke : 'black',
-            originX : 'center',
-            originY : 'center'
-        });
+        if(lastPoint == null) {
+
+            console.log("null");
+
+            var pointer = mCanvas.getPointer(o.e)
+            console.log("마우스 다운" + pointer.x + " , " + pointer.y);
+        
+            var point = [pointer.x, pointer.y, pointer.x, pointer.y];
+            line = new fabric.Line(point, {
+                id : objectId,
+                strokeWidth : 2,
+                fill : 'black',
+                stroke : 'black',
+                originX : 'center',
+                originY : 'center'
+            });
+        } else {
+            console.log("not null");
+
+            var pointer = mCanvas.getPointer(o.e)
+            console.log("마우스 다운" + pointer.x + " , " + pointer.y);
+            console.log("마지막 마우스 포인트" + lastPoint[0] + " , " + lastPoint[1]);
+    
+            var point = [lastPoint[0], lastPoint[1], lastPoint[0], lastPoint[1]];
+            line = new fabric.Line(point, {
+                id : objectId,
+                strokeWidth : 2,
+                fill : 'black',
+                stroke : 'black',
+                originX : 'center',
+                originY : 'center'
+            }); 
+        }
+        
+        
+
+        // var circle = new fabric.Circle({
+        //     left : pointer.x,
+        //     top : pointer.y,
+        //     radius : 5,
+        //     fill : '',
+        //     stroke : 'black',
+        //     originX : 'center',
+        //     originY : 'center'
+        // })
 
         console.log(objectId);
         mCanvas.add(line)
+        // mCanvas.add(circle)
 
     });
 
@@ -62,18 +113,44 @@ function setListener(){
             return;
         }
 
+
+        renderingObject = true;
+        
+
         console.log("마우스 이동");
 
-        var pointer = mCanvas.getPointer(o.e)
+        var pointer = mCanvas.getPointer(o.e);
+
+        lastPoint = [pointer.x, pointer.y];
+
         line.set({x2 : pointer.x, y2 : pointer.y});
         mCanvas.renderAll();
+    
+       
 
     });
 
     mCanvas.on('mouse:up', function(o){
         console.log("마우스 업");
         isDown = false;
-        objectId = objectId+1;
+
+        if(renderingObject == true) {
+            console.log("랜더링 만들어져쏘");
+            objectId = objectId+1;
+            renderingObject = false;
+        } else {
+            console.log("랜더링 안만들어져쏘");
+            renderingObject = false;
+
+        }
+        
+    
+    });
+
+    mCanvas.on('mouse:dblclick', function(o){
+        console.log("더블클릭");
+        lastPoint = null;
+    
     });
 
 }
